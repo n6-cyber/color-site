a509f4b3c9269639791f88214922297228421b85
n6-cyber <ribaruktimothy@gmail.com>
2026-04-22 02:29:04 +0100

    added button to index and added flex to container id, created a create grid function, working on connecting btn to createGrid()

--- javascript.js
+++ javascript.js
@@ -1,2 +1,28 @@
-let poop = "dirty";
-console.log(poop);
+const container = document.querySelector("#container");
+
+
+
+function createGrid(size) {
+
+    if (size > 100) {
+        alert("Cannot be above 100 units!");
+        return;
+    }
+
+    while (container.firstChild()) {
+        container.removeChild(container.firstChild);
+    }
+
+    let totalSize = size * size;
+
+    for (let i = 0; i < totalGridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${100 / size}%`;
+        square.style.height = `${100 / size}%`;
+
+        container.appendChild(square);
+    }
+}
+
+const btn = document.querySelector("#reset-btn");
